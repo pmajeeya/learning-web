afe3bdff3b6d8da518faf8dda9143abea52ffd20
pmajeeya <133582855+pmajeeya@users.noreply.github.com>
2023-07-24 17:49:31 +0700

    animations, pageup, sending form data.

--- module_3/src/js/script.js
+++ module_3/src/js/script.js
@@ -77,5 +77,37 @@ $(document).ready(function(){
   ValidForm('#consultation-form');
   ValidForm('#order form');
   ValidForm('#consultation form');
+  
+  $('input[name=phone]').mask("+7 (999) 999-99-99");
 
+  $('form').submit(function(e) {
+    e.preventDefault();
+    
+    if (!$(this).valid()) {
+      return;
+    }
+
+    $.ajax({
+      type: "POST",
+      url: "mailer/smart.php",
+      data: $(this).serialize()
+    }).done(function() {
+        $(this).find("input").val("");
+        $('#consultation, #order').fadeOut();
+        $('.overlay, #thanks').fadeIn('slow');
+        $('form').trigger('reset');
+    });
+    return false;
+  });
+
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 1600) {
+      $(".pageup").fadeIn('fast');
+    } else {
+      $(".pageup").fadeOut('fast');
+    }
+  });
+
+  new WOW().init();
+  
 });
