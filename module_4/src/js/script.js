c45e577d5a71bba4720d66b72c75288d37880aa1
pmajeeya <133582855+pmajeeya@users.noreply.github.com>
2023-08-07 12:41:46 +0700

    final m4
    gg

--- module_4/src/js/script.js
+++ module_4/src/js/script.js
@@ -8,3 +8,18 @@ hamburger.addEventListener('click', () => {
 closeElem.addEventListener('click', () => {
     menu.classList.remove('active');
 });
+const counters = document.querySelectorAll('.skill-measure__width'),
+      lines = document.querySelectorAll('.skill-measure__line div');
+
+counters.forEach( (item, i) => {
+    lines[i].style.width = item.innerHTML;
+});
+
+
+
+// const counters = document.querySelectorAll('.skills__ratings-counter'),
+//       lines = document.querySelectorAll('.skills__ratings-line span');
+
+// counters.forEach( (item, i) => {
+//     lines[i].style.width = item.innerHTML;
+// });
